9a5b74ab6912eea6a2a7cdbb0f078e1c11c55754
Gzh0821 <gaozih0821@outlook.com>
2022-07-09 10:31:15 +0800

    change search method.

---
+++ article/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import ArticleStorage
+
+
+class ArticlePostForm(forms.ModelForm):
+    """写文章的表单类"""
+
+    class Meta:
+        # 表明数据模型的来源
+        model = ArticleStorage
+        # 定义提交表单中的字段
+        fields = ('title', 'text')
--- article/views.py
+++ article/views.py
@@ -8,12 +8,12 @@ import markdown
 
 def test_hello_word(request):
     """A simple HelloWorld test,return USER_AGENT for example"""
-    return HttpResponse(str(request.META['HTTP_USER_AGENT']))
+    return HttpResponse("Your user agent:"+str(request.META['HTTP_USER_AGENT']))
 
 
 def show_article(request):
-    """展示所有文章"""
-    article_list = ArticleStorage.objects.all()
+    """展示所有发布的文章"""
+    article_list = ArticleStorage.objects.filter(if_publish=True)
     # 传递给模板的对象
     show_article_context = {'articles': article_list}
     return render(request, template_name='article/list.html', context=show_article_context)
@@ -22,6 +22,11 @@ def show_article(request):
 def article_detail(request, article_id):
     """获得指定id的文章"""
     selected_article = ArticleStorage.objects.get(id=article_id)
+    # 该文章不可见时进行屏蔽处理
+    if selected_article.if_publish is False:
+        response = HttpResponse("404 Not Found.Your user agent:"+str(request.META['HTTP_USER_AGENT']))
+        response.status_code = 404
+        return response
     selected_article.text = markdown.markdown(selected_article.text,
                                               extensions=[
                                                   # 缩写,表格等常用扩展
