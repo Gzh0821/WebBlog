757857febe7fd00d74b3b6841573075a9b94e7e3
Gzh0821 <gaozih0821@outlook.com>
2022-07-14 19:20:23 +0800

    change setting in release.

--- WebBlog/settings_example.py
+++ WebBlog/settings_example.py
@@ -24,7 +24,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
